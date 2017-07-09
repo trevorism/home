34879f54f71cee7ad60b2f595b8f83e05426322c
Trevor Brooks <trevorism@gmail.com>
2017-07-08 22:17:11 -0400

    Update tech radar

--- js/technology.js
+++ js/technology.js
@@ -4,15 +4,15 @@ $(function() {
     }
 
     function displayAvoids() {
-        var developmentToolsAvoid = ["Eclipse", "SVN", "Ant", "Maven", "Ansible"];
+        var developmentToolsAvoid = ["Puppet", "Eclipse", "SVN", "Ant", "Maven", "Ansible"];
         developmentToolsAvoid.forEach(function (value) {
             $("#avoid-dt").append(displayTechnology(value));
         });
-        var languagesPlatformsAvoid = ["Bash", "Perl", "Clojure", "Abap", "Coffeescript", "Flex", "XML", "PHP", "Google App Engine"];
+        var languagesPlatformsAvoid = ["Bash", "Perl", "Python", "Clojure", "Abap", "Coffeescript", "Flex", "XML", "PHP"];
         languagesPlatformsAvoid.forEach(function (value) {
             $("#avoid-lp").append(displayTechnology(value));
         });
-        var frameworksLibrariesAvoid = ["Hibernate", "Grails", "Log4j", "Swing", "Struts", "JSF"];
+        var frameworksLibrariesAvoid = ["Hibernate", "Jersey", "Grails", "Log4j", "Swing", "Struts", "JSF"];
         frameworksLibrariesAvoid.forEach(function (value) {
             $("#avoid-fl").append(displayTechnology(value));
         });
@@ -23,19 +23,19 @@ $(function() {
     }
 
     function displayEvaluates() {
-        var developmentToolsEvaluate = ["Puppet", "Powershell", "Windows DSC", "Power BI", "GoCD"];
+        var developmentToolsEvaluate = ["Powershell", "Windows DSC", "Power BI", "GoCD"];
         developmentToolsEvaluate.forEach(function (value) {
             $("#evaluate-dt").append(displayTechnology(value));
         });
-        var languagesPlatformsEvaluate = ["C#", "Scala", "Python", "R", "Ruby", "AWS", "Druid"];
+        var languagesPlatformsEvaluate = ["C#", "Scala", "R", "Kotlin", "Ruby", "AWS", "Google App Engine"];
         languagesPlatformsEvaluate.forEach(function (value) {
             $("#evaluate-lp").append(displayTechnology(value));
         });
-        var frameworksLibrariesEvaluate = ["ASP.net", "Spark", "Cucumber", "Gatling", "Node.js"];
+        var frameworksLibrariesEvaluate = ["Spark", "Cucumber", "Gatling", "Node.js"];
         frameworksLibrariesEvaluate.forEach(function (value) {
             $("#evaluate-fl").append(displayTechnology(value));
         });
-        var databaseMessageBusEvaluate = ["SqlServer", "Kafka", "Greenplum", "Cassandra", "Titan"];
+        var databaseMessageBusEvaluate = ["SqlServer", "Greenplum", "Druid", "Titan"];
         databaseMessageBusEvaluate.forEach(function (value) {
             $("#evaluate-dm").append(displayTechnology(value));
         });
@@ -50,11 +50,11 @@ $(function() {
         languagesPlatformsAdopt.forEach(function (value) {
             $("#adopt-lp").append(displayTechnology(value));
         });
-        var frameworksLibrariesAdopt = ["Spring", "Jersey/Jackson", "Logback", "Junit", "JQuery", "Bootstrap"];
+        var frameworksLibrariesAdopt = ["Spring Boot", "Logback", "Junit", "JQuery", "Bootstrap"];
         frameworksLibrariesAdopt.forEach(function (value) {
             $("#adopt-fl").append(displayTechnology(value));
         });
-        var databaseMessageBusAdopt = ["Postgres", "Redis", "HDFS", "ActiveMQ"];
+        var databaseMessageBusAdopt = ["Postgres", "Redis", "Cosmos", "Cassandra", "Kafka"];
         databaseMessageBusAdopt.forEach(function (value) {
             $("#adopt-dm").append(displayTechnology(value));
         });
