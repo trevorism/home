4790cfdd54b48cb85684b4d7ca946371af3fe31e
Trevor Brooks <trevorism@gmail.com>
2018-03-15 20:56:15 -0400

    Added dates to the articles

--- src/main/webapp/js/navbar.js
+++ src/main/webapp/js/navbar.js
@@ -10,10 +10,10 @@ $(function () {
         '                        <li><a class="headerLink" href="blogs.html">Prototype Driven Development</a></li>' +
         '                        <li><a class="headerLink" href="trends.html">Technology Trends</a></li>' +
         '                        <li><a class="headerLink" href="production.html">Productionalized Web Service</a></li>' +
-        '                        <li><a class="headerLink" href="http://registry.datastore.trevorism.com">Service Registry</a></li>' +
         '                        <li><a class="headerLink" href="technology2016.html">Technology Radar (2016)</a></li>' +
         '                        <li><a class="headerLink" href="technology2018.html">Technology Radar (2018)</a></li>' +
         '                    </ul>' +
+        '                <li><a class="headerLink" href="http://registry.datastore.trevorism.com">Service Registry</a></li>' +
         '                <li><a class="headerLink" href="contact.html">Contact</a></li>' +
         '                <li><a class="headerLink" href="careers.html">Careers</a></li>' +
         '            </ul>' +
